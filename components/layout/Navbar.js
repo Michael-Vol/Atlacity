12f191782f703716a0f3182d07f35ddabdc3cba1
Michael-Vol <mivolakis@gmail.com>
2021-12-09 10:12:12 +0200

    initial commit

---
+++ components/layout/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => {
+	return <div>{/* <nav className='navbar navbar-expand-lg navbar-light bg-light' /> */}</div>;
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { useEffect } from 'react';
+import Navbar from '../components/layout/Navbar';
 export default () => {
 	return (
 		<div>
+			<Navbar />
 			<h1> Hello World</h1>
 		</div>
 	);
