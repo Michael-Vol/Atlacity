ea80dbaea3b29586ef6465cc8ad87cefaa2531d9
Michael-Vol <mivolakis@gmail.com>
2021-12-30 19:24:09 +0200

    created email verification route

---
+++ pages/api/auth/email_verify.js
@@ -0,0 +1,77 @@
+import twilio from 'twilio';
+
+import User from '../../../models/User';
+import connectToDB from '../../../lib/db';
+import initializeMiddleware from '../../../lib/middleware/initializeMiddleware';
+import validateMiddleware from '../../../lib/middleware/validateMiddleware';
+import { check, validationResult } from 'express-validator';
+import checkAuth from '../../../lib/middleware/checkAuth';
+import getEnv from '../../../config/env';
+
+const emailVerificationHandler = async (req, res) => {
+	switch (req.method) {
+		case 'POST': {
+			try {
+				const validateBody = initializeMiddleware(
+					validateMiddleware(
+						[
+							check('code').not().isEmpty().withMessage('Verification Code is required'),
+							check('code').isNumeric().withMessage('Verification code must be a number'),
+						],
+						validationResult
+					)
+				);
+
+				await validateBody(req, res);
+
+				const email = req.user.email;
+				const code = req.body.code;
+
+				await connectToDB();
+				const user = await User.findOne({ email: req.user.email });
+
+				// Check if user's email is already verified
+				if (user.emailVerified) {
+					return res.status(400).json({
+						message: 'Email is already verified',
+					});
+				}
+				//Verify twilio code with user's code
+
+				const accountSID = getEnv('TWILIO_ACCOUNT_SID');
+				const authToken = getEnv('TWILIO_AUTH_TOKEN');
+				const emailServiceSid = getEnv('TWILIO_EMAIL_SERVICE_SID');
+				const twilioClient = twilio(accountSID, authToken);
+
+				const verification_check = await twilioClient.verify
+					.services(emailServiceSid)
+					.verificationChecks.create({
+						to: email,
+						code: code,
+					});
+
+				if (verification_check.status === 'approved') {
+					user.emailVerified = true;
+					await user.save();
+					return res.json({
+						message: 'Email Verified Successfully!',
+						verified: true,
+					});
+				}
+				return res.json({
+					message: 'Invalid verification code',
+					verified: false,
+				});
+			} catch (error) {
+				console.log(error);
+				return res.status(500).json({
+					message: 'Something went wrong',
+				});
+			}
+		}
+		default:
+			return res.status(405).json({ message: 'Invalid HTTP Method' });
+	}
+};
+
+export default checkAuth(emailVerificationHandler);
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,11 +1,13 @@
 import { check, validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
+import twilio from 'twilio';
 
 import connectToDB from '../../../lib/db';
 import initializeMiddleware from '../../../lib/middleware/initializeMiddleware';
 import validateMiddleware from '../../../lib/middleware/validateMiddleware';
 import { createAccessToken, createRefreshToken, sendRefreshToken } from '../../../lib/auth';
 import User from '../../../models/User';
+import getEnv from '../../../config/env';
 
 const validateBody = initializeMiddleware(
 	validateMiddleware(
@@ -28,53 +30,72 @@ const validateBody = initializeMiddleware(
 export default async (req, res) => {
 	switch (req.method) {
 		case 'POST': {
-			const { firstName, lastName, email, password, dateOfBirth } = req.body;
+			try {
+				const { firstName, lastName, email, password, dateOfBirth } = req.body;
 
-			await validateBody(req, res);
-			const errors = validationResult(req); //check for validation errors
+				await validateBody(req, res);
+				const errors = validationResult(req); //check for validation errors
 
-			if (!errors.isEmpty()) {
-				return res.status(422).json({ errors: errors.array() });
-			}
+				if (!errors.isEmpty()) {
+					return res.status(422).json({ errors: errors.array() });
+				}
 
-			//connect to db
-			const db = await connectToDB();
+				//connect to db
+				const db = await connectToDB();
 
-			//check if user already exists (email must be unique)
-			const existsingUser = await User.findOne({ email });
+				//check if user already exists (email must be unique)
+				const existsingUser = await User.findOne({ email });
 
-			if (existsingUser) {
-				return res.status(409).json({
-					message: 'User Already Exists',
+				if (existsingUser) {
+					return res.status(409).json({
+						message: 'User Already Exists',
+					});
+				}
+
+				//hash user password before storing it in db
+
+				const salt = await bcrypt.genSalt(10);
+				const hashedPassword = await bcrypt.hash(password, salt);
+
+				//create new user
+				const user = new User({
+					firstName,
+					lastName,
+					email,
+					dateOfBirth,
+					password: hashedPassword,
+				});
+
+				await user.save(); //save user to db
+
+				//create access and refresh tokens
+				const accessToken = createAccessToken(user);
+				const refreshToken = createRefreshToken(user);
+				sendRefreshToken(res, refreshToken);
+
+				//Create twilio email verification
+				const accountSID = getEnv('TWILIO_ACCOUNT_SID');
+				const authToken = getEnv('TWILIO_AUTH_TOKEN');
+				const emailServiceSid = getEnv('TWILIO_EMAIL_SERVICE_SID');
+				const twilioClient = twilio(accountSID, authToken);
+
+				const verification = await twilioClient.verify
+					.services(emailServiceSid)
+					.verifications.create({ to: user.email, channel: 'email' });
+				console.log(verification);
+
+				return res.status(201).json({
+					message: 'User Created!',
+					user,
+					accessToken,
+				});
+			} catch (error) {
+				console.log(error);
+				res.status(500).json({
+					message: 'Something went wrong!',
+					error: error.message,
 				});
 			}
-
-			//hash user password before storing it in db
-
-			const salt = await bcrypt.genSalt(10);
-			const hashedPassword = await bcrypt.hash(password, salt);
-
-			//create new user
-			const user = new User({
-				firstName,
-				lastName,
-				email,
-				dateOfBirth,
-				password: hashedPassword,
-			});
-
-			await user.save(); //save user to db
-
-			//create access and refresh tokens
-			const accessToken = createAccessToken(user);
-			const refreshToken = createRefreshToken(user);
-			sendRefreshToken(res, refreshToken);
-
-			return res.status(201).json({
-				message: 'User Created!',
-				user,
-				accessToken,
-			});
 		}
 		default:
 			return res.status(405).json({
