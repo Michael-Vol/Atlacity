17c469e1b15b381c7d8fce295e8957793eaa8537
Michael-Vol <mivolakis@gmail.com>
2022-01-02 00:30:19 +0200

    created update user account info route

--- pages/api/users/me.js
+++ pages/api/users/me.js
@@ -1,5 +1,7 @@
 import checkAuth from '../../../lib/middleware/checkAuth';
 import { createAccessToken, createRefreshToken, sendRefreshToken } from '../../../lib/auth';
+import connectToDB from '../../../lib/db';
+
 const meHandler = async (req, res) => {
 	switch (req.method) {
 		case 'GET':
@@ -12,6 +14,29 @@ const meHandler = async (req, res) => {
 					message: 'Successfully loaded User',
 					user: req.user,
 					accessToken,
+				});
+			} catch (error) {
+				console.log(error);
+				return res.status(500).json({
+					message: 'Something went wrong',
+				});
+			}
+		case 'PATCH':
+			try {
+				const allowedUpdates = ['firstName', 'lastName', 'email', 'dateOfBirth'];
+				const updates = Object.keys(req.body);
+				const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+				if (!isValidOperation) {
+					return res.status(400).json({
+						message: 'Invalid updates',
+					});
+				}
+				await connectToDB();
+				updates.forEach((update) => (req.user[update] = req.body[update]));
+				await req.user.save();
+				return res.json({
+					message: 'Successfully updated User',
+					user: req.user,
 				});
 			} catch (error) {
 				console.log(error);
