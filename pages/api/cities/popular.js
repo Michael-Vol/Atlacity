56bfdfb438de0120f641c29e9921a24d8c8e7ea8
Michael-Vol <mivolakis@gmail.com>
2022-01-06 12:25:25 +0200

    added get popular cities route

---
+++ pages/api/cities/popular.js
@@ -0,0 +1,25 @@
+import checkAuth from '../../../lib/middleware/checkAuth';
+import connectToDB from '../../../lib/db';
+import City from '../../../models/City';
+
+const PopularCitiesHandler = async (req, res) => {
+	switch (req.method) {
+		case 'GET':
+			await connectToDB();
+			const { limit = 4 } = req.query;
+			const popularCities = await City.find({})
+				.sort({
+					popularityIndex: -1,
+				})
+				.limit(parseInt(limit));
+
+			return res.json({
+				popularCities,
+			});
+
+		default:
+			return res.status(405).json({ message: 'Invalid HTTP Method' });
+	}
+};
+
+export default checkAuth(PopularCitiesHandler);
