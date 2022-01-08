c9813b65da3a65cc39293102b3d2b295c24faab1
Michael-Vol <mivolakis@gmail.com>
2022-01-08 17:52:10 +0200

    created middleware to validate object ids and create get place route

---
+++ pages/api/places/[placeId]/index.js
@@ -0,0 +1,33 @@
+import connectToDB from '../../../../lib/db';
+import Place from '../../../../models/Place';
+import checkAuth from '../../../../lib/middleware/checkAuth';
+import validateObjectId from '../../../../lib/middleware/validateObjectId';
+
+const placeHandler = async (req, res) => {
+	switch (req.method) {
+		case 'GET':
+			try {
+				await connectToDB();
+
+				const place = await Place.findById(req.query.placeId).populate('city');
+				if (!place) {
+					return res.status(400).json({
+						message: 'Place not found',
+					});
+				}
+
+				return res.json({
+					place,
+				});
+			} catch (error) {
+				console.log(error);
+				return res.status(500).json({
+					message: 'Something went wrong',
+				});
+			}
+		default:
+			return res.status(405).json({ message: 'Invalid HTTP method' });
+	}
+};
+
+export default checkAuth(validateObjectId(placeHandler, 'placeId'));
