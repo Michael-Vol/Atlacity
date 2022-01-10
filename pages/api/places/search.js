00682e0bec68fcf3c9f91f6481dd4bd48d17323f
Michael-Vol <mivolakis@gmail.com>
2022-01-10 17:04:13 +0200

    added PlaceSearchHandler

---
+++ pages/api/places/search.js
@@ -0,0 +1,24 @@
+import checkAuth from '../../../lib/middleware/checkAuth';
+import connectToDB from '../../../lib/db';
+import Place from '../../../models/Place';
+
+const CitySearchHandler = async (req, res) => {
+	switch (req.method) {
+		case 'GET':
+			const { name, limit = 5 } = req.query;
+			if (!name) {
+				return res.status(400).json({
+					message: 'Name is required as a query parameter',
+				});
+			}
+			await connectToDB();
+			const places = await Place.find({ name: new RegExp('^' + name, 'i') }).limit(parseInt(limit));
+			return res.json({
+				places,
+			});
+		default:
+			return res.status(405).json({ message: 'Invalid HTTP Method' });
+	}
+};
+
+export default checkAuth(CitySearchHandler);
